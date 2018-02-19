5951fec0e4add86d65de4c12638dc8c9cc29d8fb
Notsileous <phatgreendog@yahoo.com>
2018-02-18 20:09:00 -0500

    initial work on country road/background, intitial code on scrolling scripts

---
+++ LongDarkRoad/Assets/Scripts/scrolling.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrolling : MonoBehaviour {
+
+	private Transform camTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+
+
+	public float backgroundSize;
+
+	private void start(){
+		camTransform = Camera.main.transform;
+		layers = new Transform[camTransform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			layers [i] = transform.GetChild (i);
+		}
+		leftIndex = 0;
+		rightIndex = layers.Length-1;
+	}
+
+	private void scrollLeft(){
+		int lastRight = rightIndex;
+		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+		if (rightIndex < 0) {
+			rightIndex = layers.Length - 1;
+		}
+	}
+
+	private void scrollRight(){
+	}
+
+}
