7f0297d746106c7f2b9c5127be1ac901828c4a82
Ben <bliston@eginity.com>
2018-03-16 16:29:56 -0400

    added scroll to bg,road, and bus for proof of concept

---
+++ LongDarkRoad/Assets/Scripts/bus_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bus_move : MonoBehaviour {
+	public float speed = 3.0f; 
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = startPos;
+		v.x -=  (Time.time * speed);
+		transform.position = v;
+	}
+}
--- LongDarkRoad/Assets/Scripts/scrolling.cs
+++ LongDarkRoad/Assets/Scripts/scrolling.cs
@@ -9,13 +9,14 @@ public class scrolling : MonoBehaviour {
 	private float viewZone = 10;
 	private int leftIndex;
 	private int rightIndex;
-
-
+	//road = 0
+	//bg = 1
+	public int type;
 	public float backgroundSize;
 
-	private void start(){
+	private void Start(){
 		camTransform = Camera.main.transform;
-		layers = new Transform[camTransform.childCount];
+		layers = new Transform[transform.childCount];
 		for (int i = 0; i < transform.childCount; i++) {
 			layers [i] = transform.GetChild (i);
 		}
@@ -25,7 +26,10 @@ public class scrolling : MonoBehaviour {
 
 	private void scrollLeft(){
 		int lastRight = rightIndex;
-		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		layers[rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		if(type == 0){
+			layers[rightIndex].position = layers[rightIndex].position - new Vector3(0, 8, 0);
+		}
 		leftIndex = rightIndex;
 		rightIndex--;
 		if (rightIndex < 0) {
@@ -34,6 +38,27 @@ public class scrolling : MonoBehaviour {
 	}
 
 	private void scrollRight(){
+		int lastLeft = leftIndex;
+		layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+		if(type == 0){
+			layers[leftIndex].position = layers[leftIndex].position - new Vector3(0, 8, 0);
+		}
+	
+		rightIndex = leftIndex;
+		leftIndex++;
+		if(leftIndex == layers.Length){
+			leftIndex = 0;
+		}
+	}
+
+	private void Update(){
+		if(camTransform.position.x < (layers[leftIndex].transform.position.x + viewZone)){
+			scrollLeft();
+		}
+
+		if(camTransform.position.x > (layers[rightIndex].transform.position.x - viewZone)){
+			scrollRight();
+		}
 	}
 
 }
